f9bcc87baad7bf5765857c51e17511c8028e9225
br14nn <vituallabrian727@gmail.com>
2024-09-26 20:15:43 +0800

    added user model & create user endpoint

---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,6 @@
+export default class CreateUserDto {
+  id: string;
+  email: string;
+  name: string;
+  picture: string;
+}
---
+++ src/users/users.service.ts
@@ -0,0 +1,28 @@
+import { BadRequestException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto';
+import { PrismaService } from 'src/prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createUserDto: CreateUserDto) {
+    try {
+      if (createUserDto) {
+        const data = await this.prismaService.user.upsert({
+          create: createUserDto,
+          update: {
+            email: createUserDto.email,
+            name: createUserDto.name,
+            picture: createUserDto.picture,
+          },
+          where: { id: createUserDto.id },
+        });
+
+        return data;
+      } else throw Error();
+    } catch (error) {
+      throw new BadRequestException('Failed to create user');
+    }
+  }
+}
